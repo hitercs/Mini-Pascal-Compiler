f9685d6aed9924178014fb30412d8f9af8dcbbc6
CHENShuang1994 <979524237@qq.com>
2016-05-13 23:42:58 +0800

    backup the 1.0v

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char* agrv[])
 {
     Lexer lex;
     lex.token_analysis("quicksort.pas");
+    //lex.token_analysis("quicksort_emample.pas");
     //Yaccer yacc("tables.txt", "productions.txt");
     Yaccer yacc("minipas_table.txt", "pro_code.txt");
     yacc.LR_analysis("token.txt");
