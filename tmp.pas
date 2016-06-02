ff87e12b622d9bbc93758d5f09efe29790fb378c
CHENShuang1994 <979524237@qq.com>
2016-06-02 15:36:02 +0800

    start translation

--- tmp.pas
+++ tmp.pas
@@ -2,12 +2,5 @@ program quicksort(input, output);
 var 
 	array1, array2 : array[1..20] of integer;
 	tmp : real;
-function testFunc(tmp1, tmp2 : array[1..20] of real; var1 : integer) : real;
-begin
-	var1 := 10;
-end;
-procedure qsort;
-begin
-end;
 begin
 end
